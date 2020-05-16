8d2fb7b52ed2c36619be4317e1972f2feeb03dc9
I352778 <c16492454@mydit.ie>
2020-05-16 18:11:36 +0100

    Survey object added

---
+++ src/Survey.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Survey {
+
+    //Name of survey
+    private String name;
+    //Collection to represent questions
+    private ArrayList<Question> questions = new ArrayList<Question>();
+    //Collection to represent survey responses
+
+    //Empty Constructor to make blank survey
+    public Survey()
+    {
+
+    }
+
+    //Allowing me to make a survey with multiple questions and a name
+    public Survey(String name, ArrayList<Question> questions) {
+        this.name = name;
+        this.questions = questions;
+    }
+
+    //Allows me to make a survey with just a name
+    public Survey(String name) {
+        this.name = name;
+
+    }
+
+    //Add a single question to my survey
+    public void add(Question q)
+    {
+        this.questions.add(q);
+    }
+
+    //Getting name for survey
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+    public void setQuestions(ArrayList<Question> questions) {
+        this.questions = questions;
+    }
+
+}
